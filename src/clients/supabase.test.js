81fa70ca1134c403cbce1457d037ceba34343765
augustonildo <augustocarvalhopp@gmail.com>
2023-05-21 18:32:51 -0300

    Routes login tests

--- src/clients/supabase.test.js
+++ src/clients/supabase.test.js
@@ -6,8 +6,10 @@ vi.mock('@supabase/supabase-js', () => {
     createClient: () => {
       return {
         auth: {
-          user: {
-            id: 1,
+          user: () => {
+            return {
+              id: 1,
+            }
           },
           signInWithPassword: () => {
             return {
@@ -28,4 +30,15 @@ describe('AddFriendsRoute', () => {
     const result = await signIn(email, password)
     expect(result.error).toBe(null)
   })
+
+  //   it('uploadImage etc etc', async () => {
+  //     // TODO change uri to local test-image.jpg
+  //     const imageUri =
+  //       'https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.pinterest.com%2Fpin%2F606789749779588832%2F&psig=AOvVaw3SRhAPHNXJTLpmD3Emxi1x&ust=1684786651648000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCOj398Sdh_8CFQAAAAAdAAAAABAI'
+  //     const caption = 'image description'
+
+  //     const result = await uploadImage(imageUri, caption)
+  //     console.log(result)
+  //     expect(result.error).toBe(null)
+  //   })
 })
--- src/routes/login/index.test.jsx
+++ src/routes/login/index.test.jsx
@@ -1,9 +1,14 @@
-import { describe, it, expect } from 'vitest'
+import { afterEach, describe, it, expect, vi } from 'vitest'
+import * as supabase from '../../clients/supabase'
 import { screen, fireEvent } from '@testing-library/react'
 import { renderWithRouter } from '../../test-utils'
 import LoginRoute from '.'
 
 describe('LoginRoute', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   it('render the login route', () => {
     renderWithRouter(<LoginRoute />, '/login')
     const emailInput = screen.getByPlaceholderText('Email')
@@ -15,15 +20,59 @@ describe('LoginRoute', () => {
     expect(loginButton).toBeInTheDocument()
   })
 
-  it('should fill email and passowrd inputs', () => {
+  it('should fill email and password inputs', () => {
     renderWithRouter(<LoginRoute />, '/login')
     const emailInput = screen.getByPlaceholderText('Email')
     const passwordInput = screen.getByPlaceholderText('Password')
 
     fireEvent.change(emailInput, { target: { value: 'test@email.com' } })
     fireEvent.change(passwordInput, { target: { value: '123456' } })
-    
+
     expect(emailInput).toHaveValue('test@email.com')
     expect(passwordInput).toHaveValue('123456')
+  })
+
+  it('should not login with empty email and password', () => {
+    renderWithRouter(<LoginRoute />, '/login')
+    const loginButton = screen.getByText('Login')
+
+    fireEvent.click(loginButton)
+
+    const spySignIn = vi.spyOn(supabase, 'signIn').mockImplementation(() => {})
+    expect(spySignIn).toHaveBeenCalledTimes(0)
+  })
+
+  it('should not login with invalid email and password', () => {
+    renderWithRouter(<LoginRoute />, '/login')
+    const emailInput = screen.getByPlaceholderText('Email')
+    const passwordInput = screen.getByPlaceholderText('Password')
+    const loginButton = screen.getByText('Login')
+    const spySignIn = vi.spyOn(supabase, 'signIn').mockReturnValue({ error: true })
+
+    fireEvent.change(emailInput, { target: { value: 'invalid@email.com' } })
+    fireEvent.change(passwordInput, { target: { value: '0' } })
+    fireEvent.click(loginButton)
+
+    expect(spySignIn).toHaveBeenCalledTimes(1)
+    expect(emailInput).toBeInTheDocument()
+    expect(passwordInput).toBeInTheDocument()
+    expect(loginButton).toBeInTheDocument()
+  })
+
+  it('should login succesfully with correct email and password', () => {
+    renderWithRouter(<LoginRoute />, '/login')
+    const emailInput = screen.getByPlaceholderText('Email')
+    const passwordInput = screen.getByPlaceholderText('Password')
+    const loginButton = screen.getByText('Login')
+    const spySignIn = vi.spyOn(supabase, 'signIn').mockReturnValue({ error: null })
+
+    fireEvent.change(emailInput, { target: { value: 'validSignedUp@email.com' } })
+    fireEvent.change(passwordInput, { target: { value: 'strongPassword' } })
+    fireEvent.click(loginButton)
+
+    expect(spySignIn).toHaveBeenCalledTimes(1)
+    expect(emailInput).toBeInTheDocument()
+    expect(passwordInput).toBeInTheDocument()
+    expect(loginButton).toBeInTheDocument()
   })
 })
