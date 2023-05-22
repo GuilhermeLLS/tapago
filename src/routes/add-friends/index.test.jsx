5897d6d0a7e082ff8e66a92828fc22b55e008dab
augustonildo <augustocarvalhopp@gmail.com>
2023-05-21 21:38:35 -0300

    Routes tests

--- src/routes/add-friends/index.test.jsx
+++ src/routes/add-friends/index.test.jsx
@@ -1,9 +1,35 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { fireEvent, screen } from '@testing-library/react'
 import { renderWithRouter } from '../../test-utils'
+import * as requests from '../../services/requests'
 import AddFriendsRoute from '.'
 
+vi.mock('@supabase/supabase-js', () => {
+  return {
+    createClient: () => {
+      return {
+        auth: {
+          user: () => {
+            return {
+              id: 1,
+            }
+          },
+          signInWithPassword: () => {
+            return {
+              error: null,
+            }
+          },
+        },
+      }
+    },
+  }
+})
+
 describe('AddFriendsRoute', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   it('renders friends route', () => {
     renderWithRouter(<AddFriendsRoute />, '/add-friends')
     const addFriendsButton = screen.getByRole('button', { name: 'Add Friend' })
@@ -20,5 +46,36 @@ describe('AddFriendsRoute', () => {
     fireEvent.change(friendsEmailInput, { target: { value: 'friend@test.com' } })
 
     expect(friendsEmailInput).toHaveValue('friend@test.com')
+  })
+
+  it('should throw error if error happens on user search', () => {
+    const mockGetId = vi.spyOn(requests, 'getIdFromUserByEmail').mockReturnValueOnce({ error: true })
+    renderWithRouter(<AddFriendsRoute />, '/add-friends')
+    const addButton = screen.getByText('Add Friend')
+    const friendsEmailInput = screen.getByLabelText('Friends Email')
+
+    fireEvent.click(addButton)
+    expect(mockGetId).toHaveBeenCalledOnce()
+    expect(friendsEmailInput).toBeEmptyDOMElement()
+  })
+
+  it('should throw error if error happens on user search', () => {
+    const mockGetId = vi.spyOn(requests, 'getIdFromUserByEmail').mockReturnValueOnce({ error: false })
+
+    renderWithRouter(<AddFriendsRoute />, '/add-friends')
+    const addButton = screen.getByText('Add Friend')
+
+    fireEvent.click(addButton)
+    expect(mockGetId).toHaveBeenCalledOnce()
+  })
+
+  it('should return valid data from user', () => {
+    const mockGetId = vi.spyOn(requests, 'getIdFromUserByEmail').mockReturnValueOnce({ data: { id: 2 } })
+
+    renderWithRouter(<AddFriendsRoute />, '/add-friends')
+    const addButton = screen.getByText('Add Friend')
+
+    fireEvent.click(addButton)
+    expect(mockGetId).toHaveBeenCalledOnce()
   })
 })
--- src/services/requests.js
+++ src/services/requests.js
@@ -3,3 +3,16 @@ import { supabase } from '../clients/supabase'
 export const getDataFromUserId = async (id) => {
   return await supabase.from('users').select('*').eq('id', id).single()
 }
+
+export const getIdFromUserByEmail = async (email) => {
+  return await supabase.from('users').select('id').eq('email', email).single()
+}
+
+export const updateUserAddFriend = async (id, friendId) => {
+  return await supabase
+    .from('users')
+    .update({
+      friends: supabase.raw(`array_append(friends, '${friendId}')`),
+    })
+    .eq('id', id)
+}
